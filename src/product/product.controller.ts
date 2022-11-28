8cfd3ad7575410565b7b74e0a25a2cbb259a9d2c
Lucas Kahlil <kahlilpereira@gmail.com>
2022-11-28 18:12:13 -0300

    implementacao do sumary no controler do product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,7 +7,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dto/create.product.dto';
 import { Product } from './entities/product.entity';
 import { ProductService } from './product.service';
@@ -18,25 +18,40 @@ export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todos os produtos',
+  })
   findAll(): Promise<Product[]> {
     return this.productService.findAll();
   }
   @Get(':id')
+  @ApiOperation({
+    summary: 'Listar apenas um produto',
+  })
   findOne(@Param('id') id: string): Promise<Product> {
     return this.productService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar um produto',
+  })
   create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productService.create(createProductDto);
   }
 
   @Put()
+  @ApiOperation({
+    summary: 'Atualizar um produto',
+  })
   update() {
     return this.productService.update();
   }
 
   @Delete()
+  @ApiOperation({
+    summary: 'Excluir um produto',
+  })
   remove() {
     return this.productService.remove();
   }
