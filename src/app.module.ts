566db8559ddbc87e9b1af707144053b6df512f5d
Lucas Kahlil <kahlilpereira@gmail.com>
2022-11-28 20:33:41 -0300

    implementacao class validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { UserModule } from './users/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductModule, UserModule, PrismaModule],
+  imports: [ProductModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/dto/create.product.dto.ts
+++ src/product/dto/create.product.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateProductDto {
+  @IsString();
   @ApiProperty() //para mostrar no swagger o schema do dto
   tamanho: string;
 
+  @IsString();
   @ApiProperty()
   modelo: string;
 }
