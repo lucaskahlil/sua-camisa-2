e8ff23fd507daf470bca2e69c9a78172b9e45f28
Lucas Kahlil <kahlilpereira@gmail.com>
2022-12-07 21:33:23 -0300

    implementacao controler module e service order

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,12 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsUUID()
+  @ApiProperty()
+  userId: string;
+
+  @IsUUID(undefined, { each: true })
+  @ApiProperty()
+  products: string[];
+}
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { OrderService } from './order.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrderService } from './order.service';
 
+@ApiTags('order')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar uma nova ordem',
+  })
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.orderService.create(createOrderDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Buscar todas as ordens',
+  })
   findAll() {
     return this.orderService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Buscar uma ordem',
+  })
   findOne(@Param('id') id: string) {
-    return this.orderService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
+    return this.orderService.findOne(id);
   }
 }
