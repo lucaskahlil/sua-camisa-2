3bd067d986075ee4428f9ad39a8d6682531de5ac
Lucas Kahlil <kahlilpereira@gmail.com>
2022-12-09 16:14:41 -0300

    adicionado add guard

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 //para que o nest entenda que isso é um módulo precisamos chamar esse decorator
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [ProductController],
   providers: [ProductService],
 })
